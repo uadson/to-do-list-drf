a9066c2d305f7663d10df257412b0446810dda84
uadson <uadsoncastro@gmail.com>
2022-01-06 11:19:11 -0300

    criada a views

--- core/views/home_view.py
+++ core/views/home_view.py
@@ -1,8 +1,20 @@
-from django.shortcuts import HttpResponse
+from core.models import Todo
+from core.serializers import TodoSerializer
 
-from django.views.generic import View
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
-class TestView(View):
-    def get(*args, **kwargs):
-        return HttpResponse('Hello Dev')
+@api_view(['GET', 'POST'])
+def todo_list(request):
+    if request.method == 'GET':
+        todo = Todo.objects.all()
+        serializer = TodoSerializer(todo, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
