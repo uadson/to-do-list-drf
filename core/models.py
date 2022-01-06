2d13b74e4ed792066eb184c2628f8d103ad27c2a
uadson <uadsoncastro@gmail.com>
2022-01-06 11:18:10 -0300

    criado arquivo models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Base(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Todo(Base):
+    name = models.CharField(max_length=120)
+    done = models.BooleanField(default=False)
