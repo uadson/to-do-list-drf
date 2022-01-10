3a063818c77be502e6fe7f57564a387a71dc30b9
uadson <uadsoncastro@gmail.com>
2022-01-10 09:39:06 -0300

    Adicionado models views set

--- core/views/home_view.py
+++ core/views/home_view.py
@@ -1,31 +1,39 @@
 from core.models import Todo
 from core.serializers import TodoSerializer
 
-from rest_framework import generics
+from rest_framework import viewsets
+# from rest_framework import generics
 # from rest_framework.decorators import api_view
-from rest_framework.exceptions import NotFound
-from rest_framework.response import Response
-from rest_framework import status
-#from rest_framework.views import APIView
+# from rest_framework.exceptions import NotFound
+# from rest_framework.response import Response
+# from rest_framework import status
+# from rest_framework.views import APIView
 
 
+# Todos os exemplos abaixo levam ao mesmo resultado porém de modos diferentes
 
-# Simplificando (otimização)
-# Todos os exemplos abaixo levam ao mesmo resultado
+# Model ViewSets
 
-class TodoListAndCreate(generics.ListCreateAPIView):
+class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
 
-class TodoDetailChangeAndDelete(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
+# "Classes Genéricas"
+
+# class TodoListAndCreate(generics.ListCreateAPIView):
+#    queryset = Todo.objects.all()
+#    serializer_class = TodoSerializer
+#
+#
+# class TodoDetailChangeAndDelete(generics.RetrieveUpdateDestroyAPIView):
+#    queryset = Todo.objects.all()
+#    serializer_class = TodoSerializer
 
 
 # Class Based View
 
-#class TodoListAndCreate(APIView):
+# class TodoListAndCreate(APIView):
 
 #    def get(self, request):
 #        todo = Todo.objects.all()
@@ -40,7 +48,7 @@ class TodoDetailChangeAndDelete(generics.RetrieveUpdateDestroyAPIView):
 #        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
 #
 #
-#class TodoDetailChangeAndDelete(APIView):
+# class TodoDetailChangeAndDelete(APIView):
 #
 #    def get_object(self, pk):
 #        try:
